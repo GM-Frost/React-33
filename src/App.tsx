17158f308c956c3d8f6e5ff41c1320593048be8f
Frosty <nayanbastola777@gmail.com>
2023-08-23 10:05:00 -0400

    Added Husky Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import reactLogo from '@/assets/react.svg';
+import viteLogo from '@/vite.svg';
+import '@/App.css';
 import Header from '@/components/Header';
 
 function App() {
@@ -20,7 +20,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button type="submit" onClick={() => setCount((prevState) => prevState + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
